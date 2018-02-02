71acff32a922586936c98c995da3e14e955e5120
webstar1987923 <webstar1987923@hotmail.com>
2018-02-02 12:22:22 +0300

    Delete log-2018-01-30.php

--- app/logs/log-2018-01-30.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2018-01-30 23:00:27 --> 404 Page Not Found: /index
-ERROR - 2018-01-30 23:00:34 --> 404 Page Not Found: /index
